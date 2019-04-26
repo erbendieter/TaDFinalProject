2c48bceb1d45c2ec0f3e287954ca8fe3f50d4bb5
Dieter Erben <erbendieter96@gmail.com>
2019-04-26 14:19:06 -0400

    Add code for keywords vocabulary

--- dictionary.R
+++ dictionary.R
@@ -1,12 +1,47 @@
 rm(list = ls())
 relief <- read.csv('a854756.csv', stringsAsFactors=F)
-relief <- subset(relief, relevance>4)
+relief <- subset(relief, relevance>4.5)
+# Changed relevance to 4.5 to get a smaller subset to choose from
 
 mydict_list <- list()
 # 13 topics
 for (i in 1:13){
   mydict_list[[i]] <- relief$term[relief$topic==unique(relief$topic)[i]]
 }
+
+topics = unlist(unique(relief$topic))
+
+# For each topic, we have a list of terms used to describe the topic.
+# Now, we will choose 10 terms per topic. We choose the most representative terms for
+# each topic.
+
+vocabulary <- setNames(data.frame(matrix(ncol = 13, nrow = 5)), topics)
+
+# See terms per topic to choose
+
+for (i in 1:13){
+  print(topics[i])
+  print(unique(unlist(mydict_list[i])))
+}
+
+# Selecting 5 terms for each topic
+vocabulary$`Violent Civil Unrest` <- unique(unlist(mydict_list[1]))[c(2,18,24,33,45)]
+vocabulary$Shelter <- unique(unlist(mydict_list[2]))[c(3,5,12,28,35)]
+vocabulary$Food <- unique(unlist(mydict_list[3]))[c(2,6,12,14,21)]
+vocabulary$`Extreme Violence, Terrorism` <- unique(unlist(mydict_list[4]))[c(2,7,21,41,49)]
+vocabulary$Sanitation <- unique(unlist(mydict_list[5]))[c(8,9,24,25,29)]
+vocabulary$Energy <- unique(unlist(mydict_list[6]))[c(2,13,19,26,44)]
+vocabulary$`Search and Rescue` <- unique(unlist(mydict_list[7]))[c(4,7,13,19,22)]
+vocabulary$Evacuation <- unique(unlist(mydict_list[8]))[c(4,7,13,16,17)]
+vocabulary$Medical <- unique(unlist(mydict_list[9]))[c(1,8,35,91,105)]
+vocabulary$Water <- unique(unlist(mydict_list[10]))[c(3,4,17,24,27)]
+vocabulary$`Elections and Politics` <- unique(unlist(mydict_list[11]))[c(8,10,14,24,54)]
+vocabulary$Intervention <- unique(unlist(mydict_list[12]))[c(1,4,9,18,22)]
+vocabulary$`Infrastructure and Utilities` <- unique(unlist(mydict_list[13]))[c(3,4,5,6,8)]
+
+vocabulary
+
+
 noquote_list <- list()
 for (i in 1:13){
   noquote_list[i] <- noquote(relief$topic[i])
